5977ae6f84299902b2aaa76785504ce0d47bcfba
Ujjwal <ujjwalupadhyay8@gmail.com>
2021-03-02 17:13:41 +0530

    update script

--- update.sh
+++ update.sh
@@ -2,10 +2,18 @@
 
 #Usage: update.sh <app-name>
 
+echo "updating ${1} ..."
+
+
 if [[ "$1" == "zoom" ]] 
 then
-	echo "updating ${1} ..."
 	rm ~/Downloads/zoom_amd64.deb
 	wget https://us04web.zoom.us/client/latest/zoom_amd64.deb -P ~/Downloads
 	sudo dpkg -i ~/Downloads/zoom_amd64.deb
+elif [[ "$1" == "nteract" ]]
+then
+	cd ~/Downloads; 
+	rm ~/Downloads/nteract*
+	gh release download --pattern '*.deb' --repo https://github.com/nteract/nteract/
+	sudo dpkg -i ~/Downloads/nteract*.deb
 fi
