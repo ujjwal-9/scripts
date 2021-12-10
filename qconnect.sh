9d047589fa5a448e44eb56ee4614b39ff17ed44d
Ujjwal Upadhyay <ujjwalupadhyay8@gmail.com>
2021-12-10 20:14:49 +0530

    teleport updates

--- qconnect.sh
+++ qconnect.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+qure_user=ujjwal.upadhyay
+
+
 qure1=192.168.1.11
 qure3=192.168.1.13
 qure5=192.168.1.15
@@ -8,7 +11,21 @@ qure9=192.168.1.19
 qure11=192.168.1.21
 
 
-server_ip="192.168.1.$1"
-echo "Server IP: $server_ip"
 
-ssh -i ~/.ssh/qureipa ujjwal.upadhyay@$server_ip
+if [[ $1 ]] && [ $1 -eq $1 2>/dev/null ]:
+then
+  server_ip="192.168.1.$1"
+  echo "Server IP: $server_ip"
+  ssh -i ~/.ssh/qureipa $qure_user@$server_ip
+else
+  if [[ "$2" == "tunnel" ]]
+  then
+    tsh ssh -N -L $3:localhost:$3 $qure_user@$1.internal.qure.ai &
+    jobs -l
+  elif [[ "$2" == "sftp" ]]
+  then
+    sftp $qure_user@$1.internal.qure.ai
+  else
+    ssh $qure_user@$1.internal.qure.ai
+  fi
+fi
--- vpn.sh
+++ vpn.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+qure_user=ujjwal.upadhyay
+kaust_user=upadhyu
+
 if [[ "$1" == "qure" ]]
 then
-	sudo openfortivpn -c ~/qurevpn.txt --trusted-cert e7b709832a359dcc4b894f2052402764360f97dc4aa6e1d64c145ea6ee25bf07
+  if [[ "$2" == "ipa" ]]
+	then
+    sudo openfortivpn -c ~/qurevpn.txt --trusted-cert e7b709832a359dcc4b894f2052402764360f97dc4aa6e1d64c145ea6ee25bf07
+  else
+    tsh login --proxy tele.qure.ai --user $qure_user
+  fi
 elif [[ "$1" == "kaust" ]] 
 then
 	if [[ "$2" == "disconnect" ]]
