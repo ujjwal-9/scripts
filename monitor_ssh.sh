ace18907eeb86a0ae4c32f7de3841c7e078a43a0
ujjwal-9 <ujjwalupadhyay8@gmail.com>
2025-04-10 18:44:36 +0530

    update

--- monitor_ssh.sh
+++ monitor_ssh.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-while true
-do
-    echo "Active SSH connections:"
-    netstat -tn | grep ':22 ' | grep ESTABLISHED
-    echo ""
-    sleep 2
-done
+echo "Active SSH connections:"
+watch -n 1 "sudo lsof -i :22"
